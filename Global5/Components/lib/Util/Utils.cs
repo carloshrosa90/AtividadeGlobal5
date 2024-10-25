78407a386cb62b8e5d1d148309e0e28ccc17714a
carloshrosa90 <carloshrosa90@hotmail.com>
2024-10-25 10:11:19 -0300

    Atualizações

--- Global5/Components/lib/Util/Utils.cs
+++ Global5/Components/lib/Util/Utils.cs
@@ -5,6 +5,43 @@ namespace Global5.Components.lib.Util
 {
     public class Utils
     {
-        
+        public void CriarCelulaTexto(PdfPTable tabela, string texto,
+     int alinhamento = PdfPCell.ALIGN_LEFT,
+     bool negrito = false, bool italico = false,
+     int tamanhoFonte = 12, int alturaCelula = 25)
+        {
+            int estilo = iTextSharp.text.Font.NORMAL;
+            if (negrito && italico)
+            {
+                estilo = iTextSharp.text.Font.BOLDITALIC;
+            }
+            else if (negrito)
+            {
+                estilo = iTextSharp.text.Font.BOLD;
+            }
+            else if (italico)
+            {
+                estilo = iTextSharp.text.Font.ITALIC;
+            }
+
+            BaseFont fonteBase = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, false);
+            iTextSharp.text.Font fonte = new iTextSharp.text.Font(fonteBase, tamanhoFonte,
+                estilo, iTextSharp.text.BaseColor.Black);
+
+            //cor de fundo diferente para linhas pares e ímpares
+            var bgColor = iTextSharp.text.BaseColor.White;
+            if (tabela.Rows.Count % 2 == 1)
+                bgColor = new BaseColor(0.95f, 0.95f, 0.95f);
+
+            PdfPCell celula = new PdfPCell(new Phrase(texto, fonte));
+            celula.HorizontalAlignment = alinhamento;
+            celula.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
+            celula.Border = 0;
+            celula.BorderWidthBottom = 1;
+            celula.PaddingBottom = 5; //pra alinhar melhor verticalmente
+            celula.FixedHeight = alturaCelula;
+            celula.BackgroundColor = bgColor;
+            tabela.AddCell(celula);
+        }
     }
 }
